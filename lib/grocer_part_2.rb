2f834ad2ba1e102c869d5cc867d787ad0b9b53b4
Luke Thinnes <lukethinnes@yahoo.com>
2020-12-02 04:05:51 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,27 +1,3 @@
-def consolidate_cart(cart)
-  index = 0
-  new_cart = []
-
-  cart.each do |grocery_item|
-    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
-    if current_item
-      new_cart_index = 0
-      new_cart.each do |new_cart_item|
-        if new_cart_item[:item] === current_item[:item]
-          new_cart_item[:count] += 1
-        end
-        new_cart_index += 1
-      end
-    else
-      grocery_item[:count] = 1
-      new_cart << grocery_item
-    end
-    index += 1
-  end
-  new_cart
-end
-
-
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
@@ -62,7 +38,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  consolidated_cart = consoldate_cart(cart)
+  consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
 
