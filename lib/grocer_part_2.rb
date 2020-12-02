3e291f9f873a819058283f81bbaf42cea72408e8
Luke Thinnes <lukethinnes@yahoo.com>
2020-12-02 04:00:40 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -38,5 +38,18 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
+  consolidated_cart = consoldate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart)
+  final_cart = apply_clearance(couponed_cart)
 
+  total = 0
+  counter = 0
+  while counter < final_cart.length
+    total += (final_cart[counter][:price] final_cart[counter][:count])
+    counter += 1
+  end
+  if total > 100
+    total -= (total - 0.1)
+  end
+  total
 end
